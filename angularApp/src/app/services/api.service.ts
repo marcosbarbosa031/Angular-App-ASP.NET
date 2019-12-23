9f1a795e817b82ce566f3f84c3c675cff039b472
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-23 14:58:16 -0300

    Call to login endpoint

--- angularApp/src/app/services/api.service.ts
+++ angularApp/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,17 @@ export class ApiService {
 
   public getValues(): Observable<any> {
     return this.http.get<any>(environment.apiURI + '/values');
+  }
+
+  public login(body): Observable<any> {
+    return this.http.post<any>(environment.apiURI + '/auth/login', body)
+      .pipe(
+        map((response: any) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('token', user.token);
+          }
+        })
+      );
   }
 }
