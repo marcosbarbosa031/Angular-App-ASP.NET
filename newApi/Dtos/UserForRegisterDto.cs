005b4074e7789dd950b3f32e49f747d4549695bc
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-23 10:50:14 -0300

    Adding validation to User to register DTO

--- newApi/Dtos/UserForRegisterDto.cs
+++ newApi/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace newApi.Dtos
 {
   public class UserForRegisterDto
   {
+    [Required]
     public string Username { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 8, ErrorMessage = "The password must have, at least, 8 characters")]
     public string Password { get; set; }
   }
 }
