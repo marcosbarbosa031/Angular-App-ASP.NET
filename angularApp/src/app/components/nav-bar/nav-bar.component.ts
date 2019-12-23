c5430a179ce289548819f043b43665f3347fcc4b
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-23 14:59:14 -0300

    Login in and login out functionality

--- angularApp/src/app/components/nav-bar/nav-bar.component.ts
+++ angularApp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  public model: any = {};
+  
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public login() {
+    this.apiService.login(this.model).subscribe( () => {
+      console.log('Successfully logged in!');
+    }, error => {
+      console.error(error.message);
+    });
+  }
+
+  public logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out!');
+  }
+
+  public loggedIn() {
+    return !!localStorage.getItem('token');
   }
 
 }
