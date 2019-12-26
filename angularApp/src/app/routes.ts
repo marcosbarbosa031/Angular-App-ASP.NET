db6fc13d991b490873d85dcf7f8a21eb1f4caad5
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-26 11:13:18 -0300

    Creating routing module

---
+++ angularApp/src/app/routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MemberListComponent } from './components/member-list/member-list.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { MessagesComponent } from './components/messages/messages.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
