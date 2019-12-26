1c26c4edd29753541bbe6f96a8c29b5cf3164514
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-26 10:34:38 -0300

    Importing animation and dropdown module

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -19,7 +21,9 @@ import { ErrorInterceptorProvider } from './helpers/error.interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     ErrorInterceptorProvider
