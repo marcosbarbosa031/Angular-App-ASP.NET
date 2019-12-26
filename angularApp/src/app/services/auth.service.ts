55c0b3d2bedb4985a6d872a676ab1a9c6aac6046
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-26 10:07:05 -0300

    Updating auth service using jwt decoder

--- angularApp/src/app/services/auth.service.ts
+++ angularApp/src/app/services/auth.service.ts
@@ -3,11 +3,14 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private jwtHelper = new JwtHelperService();
+  private decodedToken: any;
 
   constructor(
     private http: HttpClient
@@ -24,12 +27,26 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
           }
         })
       );
   }
 
+  public loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
   public register(body: any): Observable<any> {
     return this.http.post<any>(environment.apiURI + '/auth/register', body);
+  }
+
+  public decodeToken(token: any) {
+    this.decodedToken = this.jwtHelper.decodeToken(token);
+  }
+
+  public getUsername() {
+    return this.decodedToken.unique_name;
   }
 }
