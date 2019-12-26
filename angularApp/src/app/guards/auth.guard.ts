74cf4d43eb0470e41921fd57daae76d368907de7
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-26 11:31:27 -0300

    Updating auth guard logic

--- angularApp/src/app/guards/auth.guard.ts
+++ angularApp/src/app/guards/auth.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Route } from '@angular/compiler/src/core';
+import { AlertifyService } from '../services/alertify.service';
+import { AuthService } from '../services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Route,
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+    ): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+
+    this.alertify.error('You have to be logged in to access this page!');
   }
-  
+
 }
