dec31312378364ad77a38fbec688075f3684d9f8
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-27 17:05:23 -0300

    Upfating Photos return from UserForDetailDto

--- newApi/Dtos/UserForDetailDto.cs
+++ newApi/Dtos/UserForDetailDto.cs
@@ -19,6 +19,6 @@ namespace newApi.Dtos
     public string City { get; set; }
     public string Country { get; set; }
     public string PhotoUrl { get; set; }
-    public ICollection<Photo> Photos { get; set; }
+    public ICollection<PhotosForDetailDto> Photos { get; set; }
   }
 }
