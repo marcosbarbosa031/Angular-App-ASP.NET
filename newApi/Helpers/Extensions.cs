3d7a9b7295df90b540c1fc29263f3f7cec637b2f
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-27 17:05:40 -0300

    Creating an extension function of DateTime to calculate years

--- newApi/Helpers/Extensions.cs
+++ newApi/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace newApi.Helpers
@@ -9,6 +10,15 @@ namespace newApi.Helpers
       response.Headers.Add("Application-Error", message);
       response.Headers.Add("Access-Control-Expose-HEaders", "Application-Error");
       response.Headers.Add("Access-Control-Allow-Origin", "*");
+    }
+
+    public static int CalculateAge(this DateTime theDateTime)
+    {
+      var age =  DateTime.Today.Year - theDateTime.Year;
+      // Verify if birthday already passed
+      if (theDateTime.AddYears(age) > DateTime.Today) age--;
+
+      return age;
     }
   }
 }
