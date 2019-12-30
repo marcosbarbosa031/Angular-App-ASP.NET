3b6ddcdc1bc130e65ddbbb2422eb8b4a01e8a3dc
Marcos Barbosa Filho <marcos.barbosa.filho@avanade.com>
2019-12-30 16:22:25 -0300

    Creating get user and get users methods

--- angularApp/src/app/services/user.service.ts
+++ angularApp/src/app/services/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = environment.apiURI;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users`, httpOptions);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`, httpOptions);
+  }
 }
