b5d1c76f2d1890ffe740ebbaff67e90ac28188e2
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 16:42:00 -0300

    Add HostListener to prevent closing browser with changes

--- angularApp/src/app/components/members/member-edit/member-edit.component.ts
+++ angularApp/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/services';
@@ -10,8 +10,14 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm', {static: true}) editForm: NgForm;
   public user: User;
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotifications($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private route: ActivatedRoute,
