d57bc93e0c38e0bf873fb1e77bbabe4a88f9a607
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 15:40:50 -0300

    Updating member edit resolver

--- angularApp/src/app/resolver/member-edit.resolver.ts
+++ angularApp/src/app/resolver/member-edit.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { User } from '../models/user.model';
-import { UserService, AlertifyService } from '../services';
+import { UserService, AlertifyService, AuthService } from '../services';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -9,12 +9,13 @@ import { catchError } from 'rxjs/operators';
 export class MemberEditResolver implements Resolve<User[]> {
   constructor(
     private userService: UserService,
+    private authService: AuthService,
     private router: Router,
     private alertify: AlertifyService
   ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers().pipe(
+    return this.userService.getUser(this.authService.getUserId()).pipe(
       catchError(error => {
         this.alertify.error('Problem retrieving data');
         this.router.navigate(['']);
