0bfa5d3fd54d934bf71a09a2ff508234808e5dc7
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 14:45:45 -0300

    Creating functions to initialize gallery options and get images from user

--- angularApp/src/app/components/members/member-detail/member-detail.component.ts
+++ angularApp/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -9,6 +10,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberDetailComponent implements OnInit {
   public user: User;
+  public galleryOptions: NgxGalleryOptions[];
+  public galleryImages: NgxGalleryImage[];
 
   constructor(
     private route: ActivatedRoute
@@ -16,9 +19,39 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit() {
     this.loadUser();
+    this.initializeGallery();
   }
 
   private loadUser() {
     this.user = this.route.snapshot.data.user;
+  }
+
+  private initializeGallery() {
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  private getImages() {
+    const imgUrls = [];
+    this.user.photos.forEach(photo => {
+      imgUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    });
+
+    return imgUrls;
   }
 }
