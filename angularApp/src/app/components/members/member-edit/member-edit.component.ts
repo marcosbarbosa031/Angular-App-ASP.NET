9b232a824f57f37e180619c082bb755056e16dc9
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 16:17:29 -0300

    Creating funcstions to control Form

--- angularApp/src/app/components/members/member-edit/member-edit.component.ts
+++ angularApp/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/services';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
+  public user: User;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) { }
 
   ngOnInit() {
+    this.loadUser();
+  }
+
+  public updateUser() {
+    this.alertify.success('Profile updated successfully!');
+    this.editForm.reset(this.user);
+  }
+
+  private loadUser() {
+    this.user = this.route.snapshot.data.user;
   }
 
 }
