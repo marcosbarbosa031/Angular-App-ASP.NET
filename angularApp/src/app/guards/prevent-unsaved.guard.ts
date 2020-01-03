7e1febf3114d9728df1d1a1162ac5c985f1a90bb
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 16:40:54 -0300

    Refactor prevent unsave guard

--- angularApp/src/app/guards/prevent-unsaved.guard.ts
+++ angularApp/src/app/guards/prevent-unsaved.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { MemberEditComponent } from '../components/members';
 import { CanDeactivate } from '@angular/router';
 
@@ -6,7 +6,9 @@ import { CanDeactivate } from '@angular/router';
 export class PreventUnsavedChanges implements CanDeactivate<MemberEditComponent> {
   canDeactivate(components: MemberEditComponent) {
     if (components.editForm.dirty) {
-      return confirm('Are you sure you want to exit this page? Any unsaved changes will be lost.')
+      return confirm(
+        'Are you sure you want to exit this page? Any unsaved changes will be lost.'
+      );
     }
     return true;
   }
