6f03648ec28bd9f3f7c0cc48bfeb8f91c2c93adf
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 14:33:44 -0300

    Adding ngx-gallery library

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -5,20 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { JwtModule } from '@auth0/angular-jwt';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { ErrorInterceptorProvider } from './helpers';
+import { MemberDetailResolver, MemberListResolver } from './resolver';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ErrorInterceptorProvider } from './helpers';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
 import { MemberCardComponent } from './components/members/member-card/member-card.component';
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
-import { MemberDetailResolver, MemberListResolver } from './resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -44,6 +45,7 @@ export function tokenGetter() {
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
     JwtModule.forRoot({
       config: {
         tokenGetter,
