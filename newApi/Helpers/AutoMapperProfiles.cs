7352f5b8dc84211c83d4ea88ea572c922662a179
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-03 17:05:21 -0300

    Creating a map for User and User for update DTO

--- newApi/Helpers/AutoMapperProfiles.cs
+++ newApi/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace newApi.Helpers
         .ForMember(dest => dest.Age, opt =>
           opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
       CreateMap<Photo, PhotosForDetailDto>();
+      CreateMap<UserForUpdateDto, User>();
     }
   }
 }
