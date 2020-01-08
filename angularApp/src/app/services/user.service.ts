df86342635f3e8e407edbe2061d0a63dfc7f2c5b
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-08 13:59:48 -0300

    Creating method to set main photo on user service

--- angularApp/src/app/services/user.service.ts
+++ angularApp/src/app/services/user.service.ts
@@ -25,4 +25,8 @@ export class UserService {
   updateUser(id: number, user: User) {
     return this.http.put(`${this.baseUrl}/users/${id}`, user);
   }
+
+  setMainPhoto(userId: number, photoId: number) {
+    return this.http.post(`${this.baseUrl}/users/${userId}/photos/${photoId}/setMain`, {});
+  }
 }
