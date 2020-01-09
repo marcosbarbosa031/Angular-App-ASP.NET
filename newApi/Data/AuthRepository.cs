fc0972413bd0000fe7ab889582019a3479f65e4c
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-09 11:36:48 -0300

    Returning photo alongside with the user

--- newApi/Data/AuthRepository.cs
+++ newApi/Data/AuthRepository.cs
@@ -14,7 +14,7 @@ namespace newApi.Data
       }
     public async Task<User> Login(string username, string password)
     {
-      var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+      var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Username == username);
       
       if (user == null) return null;
 
