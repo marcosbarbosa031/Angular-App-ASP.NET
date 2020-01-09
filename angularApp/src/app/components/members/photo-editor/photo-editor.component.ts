3fbaffb8eeb9da2370f56b361e3bbc50b4075ecc
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-09 16:20:19 -0300

    Create function to handle photo delete on photo editor component

--- angularApp/src/app/components/members/photo-editor/photo-editor.component.ts
+++ angularApp/src/app/components/members/photo-editor/photo-editor.component.ts
@@ -26,13 +26,25 @@ export class PhotoEditorComponent implements OnInit {
       }, error => {
         this.alertify.error(error);
       });
-    }
+  }
 
-    private setNewMainPhoto(photo) {
-      this.photos.filter(p => p.isMain === true)[0].isMain = false;
-      photo.isMain = true;
-      this.authService.changeUserPhoto(photo.url);
-      this.authService.setCurrentUserPhotoUrl(photo.url);
-      localStorage.setItem('user', JSON.stringify(this.authService.getCurrentUser()));
+  public deleteUserPhoto(photoId: number) {
+    this.alertify.confirm('Are you sure you want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.getUserId(), photoId)
+        .subscribe(() => {
+          this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+          this.alertify.success('Photo has been deleted')
+        }, error => {
+          this.alertify.error('Failed to delete de photo');
+        })
+    });
+  }
+
+  private setNewMainPhoto(photo) {
+    this.photos.filter(p => p.isMain === true)[0].isMain = false;
+    photo.isMain = true;
+    this.authService.changeUserPhoto(photo.url);
+    this.authService.setCurrentUserPhotoUrl(photo.url);
+    localStorage.setItem('user', JSON.stringify(this.authService.getCurrentUser()));
   }
 }
