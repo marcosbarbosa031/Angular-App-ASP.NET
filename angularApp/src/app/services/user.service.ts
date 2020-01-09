74803ba471385b49aa78a659d3bc8274edec8cf5
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-09 16:19:32 -0300

    Creating method to delete photo in user service

--- angularApp/src/app/services/user.service.ts
+++ angularApp/src/app/services/user.service.ts
@@ -29,4 +29,8 @@ export class UserService {
   setMainPhoto(userId: number, photoId: number) {
     return this.http.post(`${this.baseUrl}/users/${userId}/photos/${photoId}/setMain`, {});
   }
+
+  deletePhoto(userId: number, photoId: number) {
+    return this.http.post(`${this.baseUrl}/users/${userId}/photos/${photoId}/delete`, {});
+  }
 }
