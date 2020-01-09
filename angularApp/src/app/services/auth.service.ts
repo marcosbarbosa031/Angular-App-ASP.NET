5b5e8bb9ba9030a48101b68a78d1c748c57ad75a
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-09 14:14:38 -0300

    Creating current user and setting and getting on login

--- angularApp/src/app/services/auth.service.ts
+++ angularApp/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class AuthService {
   private jwtHelper = new JwtHelperService();
   private decodedToken: any;
   private baseUrl = environment.apiURI;
+  private currentUser: User;
 
   constructor(
     private http: HttpClient
@@ -25,10 +27,11 @@ export class AuthService {
     return this.http.post<any>(`${this.baseUrl}/auth/login`, body)
       .pipe(
         map((response: any) => {
-          const user = response;
-          if (user) {
-            localStorage.setItem('token', user.token);
-            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          if (response) {
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('user', JSON.stringify(response.user))
+            this.decodedToken = this.jwtHelper.decodeToken(response.token);
+            this.currentUser = response.user;
           }
         })
       );
@@ -47,10 +50,24 @@ export class AuthService {
     this.decodedToken = this.jwtHelper.decodeToken(token);
   }
 
-  public getUsername() {
+  public getUsername(): string {
     return this.decodedToken.unique_name;
   }
-  public getUserId() {
+
+  public getUserId(): number {
     return this.decodedToken.nameid;
+  }
+
+  public getCurrentUser(): User {
+    return this.currentUser;
+  }
+
+  public setCurrentUser(user: User) {
+    this.currentUser = user;
+  }
+
+  public loggOut() {
+    this.decodeToken = null;
+    this.currentUser = null;
   }
 }
