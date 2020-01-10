dd02c1b8b87c6bee7a513967fc66c315839450a9
Marcos Antonio Barbosa Filho <marcos.barbosa.filho@avanade.com>
2020-01-10 11:14:34 -0300

    Creating reactforms and validatinf fields

--- angularApp/src/app/components/register/register.component.ts
+++ angularApp/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/services';
 import { AlertifyService } from 'src/app/services';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +11,8 @@ import { AlertifyService } from 'src/app/services';
 export class RegisterComponent implements OnInit {
   @Output() showRegister = new EventEmitter();
   public model: any = {};
+  public registrationForm: FormGroup;
+  public get f() { return this.registrationForm.controls; }
 
   constructor(
     private authService: AuthService,
@@ -17,19 +20,34 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.createForm();
+    console.log('form: ', this.f.password.value);
   }
 
   public register() {
-    this.authService.register(this.model).subscribe(resp => {
-      this.alertify.success('User registred successfully!');
-      this.showRegister.emit(false);
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authService.register(this.model).subscribe(resp => {
+    //   this.alertify.success('User registred successfully!');
+    //   this.showRegister.emit(false);
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+    console.log('registerForm: ', this.registrationForm.value);
   }
 
   public cancel() {
     this.showRegister.emit(false);
   }
 
+
+  private createForm() {
+    this.registrationForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)] ),
+      confirmPassword: new FormControl('', Validators.required)
+    }, this.passwordMatchValidator);
+  }
+
+  public passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { 'mismatch': true };
+  }
 }
